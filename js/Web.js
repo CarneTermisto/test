3be8c00ab37049a0c0284955d5cfd34c2559bed0
Student <aiden1pearce12@gmail.com>
2019-12-26 18:50:50 +0400

    first commit

---
+++ js/Web.js
@@ -0,0 +1,59 @@
+    $(document).ready(function(){
+
+    new WOW().init(); 
+    $(".owl-carousel").owlCarousel({
+            autoplay:true,
+            loop:true,
+            margin:10,
+            nav:true,
+            dots:false,
+            autoplayTimeout:3000,
+            slideTransition:'300ms',
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:1
+                }
+            }
+        }
+    );
+  });
+
+
+
+  function openName(evt, Name) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(Name).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+
+
+
+function openContent(evt, content) {
+    var i, tabcontent1, tablinks1;
+    tabcontent1 = document.getElementsByClassName("tabcontent1");
+    for (i = 0; i < tabcontent1.length; i++) {
+      tabcontent1[i].style.display = "none";
+    }
+    tablinks1 = document.getElementsByClassName("tablinks1");
+    for (i = 0; i < tablinks1.length; i++) {
+      tablinks1[i].className = tablinks1[i].className.replace(" active", "");
+    }
+    document.getElementById(content).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
